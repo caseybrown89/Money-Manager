9a9bd372847e63097a6d3823ef6620e1f5c8652e
Casey Brown <caseybrown89@gmail.com>
2012-04-03 11:07:19 -0400

    Weird bug when first item in Balances List doesn't have balance, displaying null when should be No Entries, added debug code, still working on it

--- src/com/caseynbrown/moneymanager/TextBalanceView.java
+++ src/com/caseynbrown/moneymanager/TextBalanceView.java
@@ -18,24 +18,24 @@ public class TextBalanceView extends LinearLayout {
 		super(c);
 
 		LayoutInflater i = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		
+
 		if (! tb.getDate().equals("")){
 			/* We are displaying a balance view of entries for a specific person */
-			
+
 			this.addView(i.inflate(R.layout.individualbalancelist, null));
-			
+
 			this.mDate = (TextView) findViewById(R.id.indivList_date);
 			this.mName = (TextView) findViewById(R.id.indivList_title);
 			this.mAmount = (TextView) findViewById(R.id.indivList_amount);
-			
+
 			this.mDate.setText(tb.getDate());
 			this.mName.setText(tb.getName());
-			
+
 			/* Set amount */
 			int amount = tb.getBalance();
 			String formatted = HelperMethods.intToDollar(amount);
 			this.mAmount.setText(formatted);
-			
+
 			/* Change color of balance accordingly */
 			if (amount < 0){
 				this.mAmount.setTextColor(Color.RED);
@@ -44,34 +44,34 @@ public class TextBalanceView extends LinearLayout {
 			}
 		} else {
 			/* We are displaying the balance view of all people */
-			
+
 			this.addView(i.inflate(R.layout.balanceviewlist, null));
-			
+
 			this.mName = (TextView) findViewById(R.id.balancelist_rowName);
 			this.mAmount = (TextView) findViewById(R.id.balancelist_rowAmount);
 			this.mLatest = (TextView) findViewById(R.id.balancelist_entryDateWhere);
-			
+			this.mLatestAmount = (TextView) findViewById(R.id.balancelist_entryAmount);
+
 			/* Set name */
 			mName.setText(tb.getName());
-			
+
 			/* Set amount */
 			int amount = tb.getBalance();
 			String formatted = HelperMethods.intToDollar(amount);
 			mAmount.setText(formatted);
-			
+
 			/* Change color of balance accordingly */
 			if (amount < 0){
 				mAmount.setTextColor(Color.RED);
 			} else {
 				mAmount.setTextColor(Color.parseColor(getResources().getString(R.string.defaultGreenColor)));
 			}
-			
+
 			/* Set Latest entry information */
 			if (tb.getLatestDate() != null){
 				String latest = tb.getLatestDate()+" "+tb.getLatestWhere();
 				mLatest.setText(latest);
-				
-				this.mLatestAmount = (TextView) findViewById(R.id.balancelist_entryAmount);
+
 				int amountLatestEntry = tb.getLatestAmount();
 				mLatestAmount.setText(HelperMethods.intToDollar(amountLatestEntry));
 				if (amountLatestEntry < 0){
@@ -81,12 +81,16 @@ public class TextBalanceView extends LinearLayout {
 				}
 			} else {
 				/* Sets the 'No Entries' to italics */
+				String date = getResources().getString(R.string.balancelist_noentry);
 				this.mLatest.setTypeface(null, Typeface.ITALIC);
+				String test1 = this.mLatest.getText().toString();
+				String test2 = this.mLatestAmount.getText().toString();
+				this.mLatestAmount.setText("");
 			}
 		}
 	}
 
-	
+
 	public void setName(String name){
 		mName.setText(name);
 	}
@@ -98,11 +102,11 @@ public class TextBalanceView extends LinearLayout {
 	public void setAmount(int amt){
 		mAmount.setText(HelperMethods.intToDollar(amt));
 	}
-	
+
 	public void setLatest(String date, String where){
 		mLatest.setText(date+" "+where);
 	}
-	
+
 	public void setLatestAmount(int amount){
 		mLatestAmount.setText(HelperMethods.intToDollar(amount));
 	}
--- src/com/caseynbrown/moneymanager/ViewBalanceActivity.java
+++ src/com/caseynbrown/moneymanager/ViewBalanceActivity.java
@@ -3,8 +3,8 @@ package com.caseynbrown.moneymanager;
 import static android.provider.BaseColumns._ID;
 import static com.caseynbrown.moneymanager.DBConstants.AMOUNT_PEOPLE;
 import static com.caseynbrown.moneymanager.DBConstants.NAME_PEOPLE;
-import static com.caseynbrown.moneymanager.DBConstants.TABLE_NAME_PEOPLE;
 import static com.caseynbrown.moneymanager.DBConstants.TABLE_NAME_ENTRY;
+import static com.caseynbrown.moneymanager.DBConstants.TABLE_NAME_PEOPLE;
 import static com.caseynbrown.moneymanager.DBConstants.USER_ENTRY;
 
 import java.util.ArrayList;
@@ -24,7 +24,7 @@ import android.widget.ListView;
 public class ViewBalanceActivity extends ListActivity {
 	/* DB Constants */
 	private static String[] FROM = {_ID, NAME_PEOPLE, AMOUNT_PEOPLE, };
-	private static String ORDER_BY = NAME_PEOPLE + " DESC";
+	private static String ORDER_BY = NAME_PEOPLE + " ASC";
 
 	private DBData people;
 	private SQLiteDatabase db;
@@ -63,16 +63,16 @@ public class ViewBalanceActivity extends ListActivity {
     	super.onResume();
 
         this.idList = new ArrayList<Integer>();
-    	
+
     	this.people = new DBData(this);
     	this.db = people.getReadableDatabase();
     	populateList();
     }
-    
-    @Override 
+
+    @Override
     public void onPause(){
     	super.onPause();
-    	
+
     	this.db.close();
     	this.people.close();
 	}
@@ -97,26 +97,26 @@ public class ViewBalanceActivity extends ListActivity {
     		idList.add(id);
     		String name = peopleCursor.getString(1);
     		int amount = peopleCursor.getInt(2);
-    		
+
     		/* Get that person's latest entry */
     		String query = "SELECT * FROM "+TABLE_NAME_ENTRY+" WHERE "+USER_ENTRY+" = "+id+" ORDER BY "+_ID+" DESC LIMIT 1";
     		Cursor latestEntry = this.db.rawQuery(query, null);
-    		
+
     		String latestDate = null;
     		String latestWhere = null;
     		int latestAmount = 0;
-    		
+
     		if (latestEntry.moveToNext()){
     			latestDate = latestEntry.getString(2);
     			latestWhere = latestEntry.getString(3);
     			latestAmount = latestEntry.getInt(4);
     		}
-    		
+
     		/* Add the entry to the list */
     		this.bal.addItem(new TextBalance(id, name, amount, latestDate, latestWhere, latestAmount));
     		latestEntry.close();
     	}
-    	
+
     	peopleCursor.close();
 	}
 
